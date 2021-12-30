9df4ac2f72f6c0465340e6980adeb61a137b06c9
Richard Puth <rtpkang@Richards-MacBook-Pro.local>
2021-12-29 19:11:08 -0500

    added bare minimum for instance deployment test

--- main.tf
+++ main.tf
@@ -9,15 +9,16 @@ terraform {
 }
 
 provider "aws" {
-    region = "us-east-1"
+    region = "us-east-2"
     shared_credentials_file = "~/.aws/credentials"
 }
-
 
 # resource "<provider>_<resource_type>" "name(scoped to terraform)" {
 #     config options....
 #     key = "value"
 # }
+
 resource "aws_instance" "riley" {
-  
+    ami = "ami-0fb653ca2d3203ac1"
+    instance_type = "t2.micro"
 }
