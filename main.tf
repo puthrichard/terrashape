c9348b4ece84df3db3f4d7072f2a864ae2f23d80
Richard Puth <rtpkang@Richards-MacBook-Pro.local>
2022-01-19 01:27:33 -0500

    Updated terraform with new concepts learned.

--- main.tf
+++ main.tf
@@ -8,17 +8,156 @@ terraform {
     }
 }
 
+variable "subnet_prefix" {
+    description = "cidr block for subnet"
+}
+
+variable "obj" {
+    description = "Object with cidr_block and name"
+}
+
 provider "aws" {
     region = "us-east-2"
     shared_credentials_file = "~/.aws/credentials"
 }
 
-# resource "<provider>_<resource_type>" "name(scoped to terraform)" {
-#     config options....
-#     key = "value"
-# }
+# Create VPC
+resource "aws_vpc" "prod_vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "production"
+    }
+}
 
-resource "aws_instance" "riley" {
+# Create Internet Gateway
+resource "aws_internet_gateway" "gw" {
+    vpc_id = aws_vpc.prod_vpc.id
+}
+
+# Create a Custom Route Table
+resource "aws_route_table" "prod_route_table" {
+  vpc_id = aws_vpc.prod_vpc.id 
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod"
+  }
+}
+
+# Create a Subnet
+resource "aws_subnet" "subnet_1" {
+    vpc_id = aws_vpc.prod_vpc.id
+    cidr_block = var.obj[0].cidr_block
+    availability_zone = "us-east-2a"
+
+    tags = {
+        Name = var.obj[0].name
+    }
+}
+
+resource "aws_subnet" "subnet_2" {
+    vpc_id = aws_vpc.prod_vpc.id
+    cidr_block = var.obj[1].cidr_block
+    availability_zone = "us-east-2a"
+
+    tags = {
+        Name = var.obj[1].name
+    }
+}
+
+# Associate subnet with Rotue Table
+resource "aws_route_table_association" "a" {
+    subnet_id = aws_subnet.subnet_1.id
+    route_table_id = aws_route_table.prod_route_table.id
+}
+
+# Create a Security Group to allow port 22, 80, 443
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.prod_vpc.id
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+# Create a Network Interface with an ip in the subnet that was created in step 4
+resource "aws_network_interface" "webserver-nic" {
+    subnet_id = aws_subnet.subnet_1.id
+    private_ips = ["10.0.1.50"]
+    security_groups = [aws_security_group.allow_web.id]
+}
+
+# Assign an elastic IP to the network interface created in step 7
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.webserver-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [aws_internet_gateway.gw]
+}
+
+# Create Ubun tu server and install/enable apache2
+resource "aws_instance" "web_server_instance" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
+    availability_zone = "us-east-2a"
+    key_name = "terra-key"
+
+    network_interface {
+        device_index = 0
+        network_interface_id = aws_network_interface.webserver-nic.id
+    }
+
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl start apache2
+                sudo bash -c 'echo your very first server > /var/www/html/index.html'
+                EOF
+    tags = {
+        Name = "webserver"
+    }
+}
+
+output "server_public_ip" {
+    value = "aws_instance.web_server_instance.public_ip"
 }
---
+++ terraform.tfvars
@@ -0,0 +1,12 @@
+subnet_prefix = ["10.0.1.0/24","10.0.2.0/24"]
+
+obj = [
+    {
+        cidr_block = "10.0.1.0/24",
+        name = "prod_subnet"
+    },
+    {
+        cidr_block = "10.0.2.0/24",
+        name = "dev_subnet"
+    }
+]
